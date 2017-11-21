af007b2a718b046e91a5ef0af5d6607a9d8e3f47
Simon Ayzman <simonayzman@gmail.com>
2017-11-21 12:51:25 -0500

    Formatted svm solution

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -21,10 +21,5 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 
-
-#########################################################
-### your code goes here ###
-
-#########################################################
 
 
